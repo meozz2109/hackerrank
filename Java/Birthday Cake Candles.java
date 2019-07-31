4b6f7134136bd7624ed88798f90dd7f413500bba
Minh Do <35888519+meozz2109@users.noreply.github.com>
2019-07-31 19:51:39 +0700

    Update Birthday Cake Candles.java

--- Java/Birthday Cake Candles.java
+++ Java/Birthday Cake Candles.java
@@ -1,4 +1,5 @@
 public static int birthdayCakeCandles(int[] inputArr){
+        //convert an integer array into a integer list (array list) and find max of collections
         List<Integer> l = new ArrayList<>();
         for(int i: inputArr) l.add(i);
         int re = Collections.max(l), count=0;
