be8ca6bf5239548f17dcb8f77b01440e85413625
meozz2109 <minhstd99@gmail.com>
2019-08-07 11:10:14 +0700

    try to refactor the current folder by deleting old folder.

--- Java/Birthday Cake Candles.java
+++
@@ -1,9 +0,0 @@
-public static int birthdayCakeCandles(int[] inputArr){
-        //convert an integer array into a integer list (array list) and find max of collections
-        List<Integer> l = new ArrayList<>();
-        for(int i: inputArr) l.add(i);
-        int re = Collections.max(l), count=0;
-        for(int i=0;i<l.size();i++)
-            if(l.get(i).equals(re)) count++;
-        return count;
-    }
