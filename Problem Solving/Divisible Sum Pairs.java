41cbe29c6b4a291d66929380d1452bb0f3f7ae78
Minh Do <35888519+meozz2109@users.noreply.github.com>
2019-08-22 17:36:35 +0700

    Update Divisible Sum Pairs.java

--- Problem Solving/Divisible Sum Pairs.java
+++ Problem Solving/Divisible Sum Pairs.java
@@ -3,9 +3,9 @@
 // Output: 5
 static int divisibleSumPairs(int n, int k, int[] ar) {
         int count=0;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++)
-                if((ar[i]+ar[j])%k==0) count++;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++)
+                if( (ar[i]+ar[j]) % k == 0) count++;
         }
         return count;
     }
