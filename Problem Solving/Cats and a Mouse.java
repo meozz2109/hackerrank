6b2e72a3507cfe07f4468bb46d0eb0205972ca8e
Minh Do <35888519+meozz2109@users.noreply.github.com>
2019-09-06 01:16:07 +0000

    Create Cats and a Mouse.java

---
+++ Problem Solving/Cats and a Mouse.java
@@ -0,0 +1,10 @@
+// Cats and a Mouse
+// For example, input data: 1 2 3
+// Expected output: Mouse B
+static String catAndMouse(int x, int y, int z) {
+        String re = "";
+        if(Math.abs(x-z)>Math.abs(y-z)) re = "Cat B";
+        else if(Math.abs(x-z)<Math.abs(y-z)) re = "Cat A";
+        else re ="Mouse C";
+        return re;
+    }
